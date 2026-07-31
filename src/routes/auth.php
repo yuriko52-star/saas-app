a7f422dc452c1a7870e10dd9e4e0ced0b6dffd42
yuriko <yuriko40223@gmail.com>
2026-07-31 17:41:12 +0900

    login

--- src/routes/auth.php
+++ src/routes/auth.php
@@ -18,10 +18,6 @@ Route::post('/login', [AuthenticatedSessionController::class, 'store'])
 
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
     ->middleware('guest')
-    ->name('password.email');
-
-Route::post('/reset-password', [NewPasswordController::class, 'store'])
-    ->middleware('guest')
     ->name('password.store');
 
 Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
