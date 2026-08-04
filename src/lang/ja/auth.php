ab39840fe9e49aaa3c0bf585560360fbacbb2e7a
yuriko <yuriko40223@gmail.com>
2026-08-04 21:44:51 +0900

    ログイン認証

---
+++ src/lang/ja/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'メールアドレスかパスワードが正しくありません。',
+    'password' => 'パスワードが正しくありません。',
+    'throttle' => ':seconds秒後にもう一度お試しください。',
+
+];
